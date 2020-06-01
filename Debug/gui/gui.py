64f917020fd8e4d9754495767447f0493925546e
gl00man <maciekbereda46@gmail.com>
2020-06-01 15:39:01 +0200

    add: stram function

--- Debug/gui/gui.py
+++ Debug/gui/gui.py
@@ -10,45 +10,13 @@ from urllib.request import urlopen as uReq #biblitoeka do nawiązywania połącz
 window = tkinter. Tk()
 window.title("Games_prices")
 
-
-print("dupcia")
-
 def get_url():
 	game_url = textbox.get()
 	textbox.delete(0, END)
 	value = combobox.get()
+
 	if(value == "Steam"):
-		table = []
-
-		#game_url= input("Steam game link: ") #pobieramy link do gry od użytkownika i zapisujemy w zmiennej game_url
-
-		uClient = uReq(game_url) #nawiązujemy połączenie ze stronką pod linkiem który znajduje się w zmiennej game_url
-		page_html = uClient.read() #zapisujemy do zmiennej page_html kod html tej strony do której się połączyliśmy
-		uClient.close() #jak mamy juz kod html z którego korzystać będzie biblioteka bs4 to zamykamy połączenie ze stroną
-
-		page_soup = soup(page_html, "html.parser") #parsujemy kod html do zmiennej page_soup
-
-		game_name = page_soup.findAll("div", {"class":"apphub_AppName"}) #zapisujemy do zmiennej game_name linijke html który zawiera div z klasą "apphub_AppName"
-		game_price = page_soup.findAll("div", {"class":"game_purchase_price price"}) #to samo działanie co powyżej
-
-
-		for p in game_name: #ta pętla służy do wyciągnięcia z tej linijki html kawałka tekstu który nas interesuje
-			game_name = p.text
-
-
-		for p in game_price:#to samo co poœyższa petla
-			game_price = p.text
-			game_price = game_price.strip() #jeżeli nawali nam niepotrzebnych spacji w stringu w którym mamy nasz tekst to używamy na nim maetody stripe() aby się ich pozbyć
-			table.append(game_price)
-
-
-		#print(game_name) #wypisujemy nasz tytuł
-		#print(table[0]) #wypisujemy cenkę tytułu
-
-		ready = game_name + " " + value + " " + table[0] #robimy stringa z wynikami naszego wyszukiwania
-		saveToFile(ready)
-
-		getTitles()
+		steamScrap(game_url, value)
 
 	elif(value == "Epic Games"):
 		print("sss")
@@ -92,6 +60,33 @@ def getTitles():
 	with open("data.txt") as f:
 		label1 = Label(window, text = f.read())
 		label1.grid(row = 4, column = 2)
+
+def steamScrap(game_url, value):
+	table = []
+
+	uClient = uReq(game_url) #nawiązujemy połączenie ze stronką pod linkiem który znajduje się w zmiennej game_url
+	page_html = uClient.read() #zapisujemy do zmiennej page_html kod html tej strony do której się połączyliśmy
+	uClient.close() #jak mamy juz kod html z którego korzystać będzie biblioteka bs4 to zamykamy połączenie ze stroną
+
+	page_soup = soup(page_html, "html.parser") #parsujemy kod html do zmiennej page_soup
+
+	game_name = page_soup.findAll("div", {"class":"apphub_AppName"}) #zapisujemy do zmiennej game_name linijke html który zawiera div z klasą "apphub_AppName"
+	game_price = page_soup.findAll("div", {"class":"game_purchase_price price"}) #to samo działanie co powyżej
+
+
+	for p in game_name: #ta pętla służy do wyciągnięcia z tej linijki html kawałka tekstu który nas interesuje
+		game_name = p.text
+
+
+	for p in game_price:#to samo co poœyższa petla
+		game_price = p.text
+		game_price = game_price.strip() #jeżeli nawali nam niepotrzebnych spacji w stringu w którym mamy nasz tekst to używamy na nim maetody stripe() aby się ich pozbyć
+		table.append(game_price)
+
+	ready = game_name + " " + value + " " + table[0] #robimy stringa z wynikami naszego wyszukiwania
+	saveToFile(ready)
+
+	getTitles()
 
 def checkPrices():
 	print("ss")
