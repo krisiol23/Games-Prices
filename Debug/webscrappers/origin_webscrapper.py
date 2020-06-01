f33846bbdbc8bd84909f2236c76a97f423fd5d58
PI3R23 <vladimirxputin@wp.pl>
2020-06-01 15:33:28 +0000

    origin z dzialajaca nazwa gry

--- Debug/webscrappers/origin_webscrapper.py
+++ Debug/webscrappers/origin_webscrapper.py
@@ -2,11 +2,12 @@
 from bs4 import BeautifulSoup as soup
 import requests
 from selenium import webdriver
+from urllib.request import urlopen as uReq
 print ("dupa")
 
 
 #game_url = input("Origin game link: ")
-game_url = 'https://www.origin.com/pol/en-us/store/fifa/fifa-20'
+game_url = 'https://www.origin.com/pol/en-us/store/battlefield/battlefield-v/'
 
 print("elo")
 
@@ -14,16 +15,39 @@ print("elo")
 #page_soup = soup(source, 'lxml')
 
 
+game_name = game_url
+game_name = game_url.replace(game_url[0:39],"")
+game_name = game_name.replace("/"," ")
+#print (game_name)
+game_name = game_name.split()
+#print (game_name)
+g4me_name = game_name[1].capitalize()
+print(g4me_name)
+
+game_price = game_url + "interstitial"
+
+uClient = uReq(game_price)
+page_html = uClient.read()
+uClient.close()
+page_soup = soup(page_html, "html.parser")
+
+
+game_price = page_soup.findAll("div", {"class":"otkex-priceblock-pricewrapper"})
+print(game_price)
+
+
+
+
 #[d for d in soup.findAll('div') if not d.find('div')]
 
-driver = webdriver.PhantomJS(executable_path='/home/malpa/Pobrane/phantomjs-2.1.1-linux-x86_64/bin/')
-driver.get(game_url)
+#driver = webdriver.PhantomJS(executable_path='/home/malpa/Pobrane/phantomjs-2.1.1-linux-x86_64/bin/')
+#driver.get(game_url)
 print(2222)
-bsObj = soup(driver.page_source,'html.parser')
-p_element = driver.find_element_byclassname('otkex-product-hero-leftrail-text')
+#bsObj = soup(driver.page_source,'html.parser')
+#p_element = driver.find_element_byclassname('otkex-product-hero-leftrail-text')
 #print(p_element.text)
 
 
 
-game_price = page_soup.findAll("div", {"class":"game_purchase_price price"})
+#game_price = page_soup.findAll("div", {"class":"game_purchase_price price"})
 
