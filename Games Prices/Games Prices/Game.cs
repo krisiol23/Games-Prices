6ec0239ceed75699c48890213d9f02c6b17f6458
gl00man <maciekbereda46@gmail.com>
2020-07-08 01:30:46 +0200

    New gui commit

---
+++ Games Prices/Games Prices/Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Games_Prices
+{
+    public class Game
+    {
+        private string title { get; set; }
+        private string platform { get; set; }
+        private string price { get; set; }
+        private string actualPrice { get; set; }
+
+        public Game(string t, string p, string pr, string ap)
+            {
+            title = t;
+            platform = p;
+            pr = price;
+            actualPrice = ap;
+            }
+
+        public override string ToString()
+        {
+            return title + " " + platform + " " + actualPrice;
+        }
+    }
+}
---
+++ Games Prices/Games Prices/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Games_Prices
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Game> GamesList;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            GamesList = new List<Game>();
+        }
+
+        private void StackPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                DragMove();
+            }
+            catch
+            {
+
+            }
+        }
+
+        //Reading Data
+        private void ReadData()
+        {
+            GamesList.Clear();
+
+            using (StreamReader sr = new StreamReader("data.txt"))
+            {
+                string line;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] par = line.Split('|');
+
+                    Game game = new Game(par[0], par[1], par[2], par[3]);
+
+                    if (!GamesList.Contains(game))
+                    {
+                        GamesList.Add(game);
+                    }
+                }
+            }
+
+            //Buttons 
+
+            private void exitBtn_Click(object sender, RoutedEventArgs e)
+            {
+                this.Close();
+            }
+
+            private void minimizeBtn_Click(object sender, RoutedEventArgs e)
+            {
+                this.WindowState = WindowState.Minimized;
+            }
+
+            private void addGameBtn_Click(object sender, RoutedEventArgs e)
+            {
+                MessageBox.Show("dasdsa");
+            }
+
+            private void delGameBtn_Click(object sender, RoutedEventArgs e)
+            {
+
+            }
+        }
+    }
+}
